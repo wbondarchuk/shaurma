798ab6631f46ef36495ae936f9446302d49681ab
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2022-05-14 21:07:35 +0700

    Add files via upload
    Стоит мой токен, надо его поменять перед запуском
    ДОБАВИЛ эмодзи, цену, график работы и прочие красивости

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
-from email import message
 from threading import Thread
 
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-
 import telebot
 import shaurma
-import  time
+import time
 
 # todo init
-token = '2097318317:AAE-6SFnxE8TOzRP6kG6iPKssa-UV_fIDQg'
+token = '5278878177:AAFD_eYJ39E3mBH-8Nv6lCF5v4KTbakBF0o'
 bot = telebot.TeleBot(token)
 cur = 0
 finished = False
 order_list = []
 to_do = []
 vidacha = []
+
+heart_symbol = u"\u2764"
+cheese_symbol = u"\U0001F9C0"
+fitness_symbol = u"\U0001F3C3\u200D\u2642\uFE0F"
+chicken_symbol = u"\U0001F357"
+pekin_symbol = u"\U0001F96C"
+burger_symbol = u"\U0001F354"
+cucumber_symbol = u"\U0001F952"
+peper_symbol = u"\U0001F336"
+bread_symbol = u"\U0001F35E"
+fries_symbol = u"\U0001F35F"
+tomato_symbol = u"\U0001F345"
+fire_symbol = u"\U0001F525"
+pineapple_symbol = u"\U0001F34D"
+corn_symbol = u"\U0001F33D"
+
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
@@ -32,7 +46,6 @@ class Ui(QtWidgets.QMainWindow):
         num_sh = int(self.editTodo.text())
         vidacha.append(to_do[num_sh - 1])
         del to_do[num_sh - 1]
-
 
     def from_vl(self):
         num_sh = int(self.editV.text())
@@ -49,38 +62,30 @@ class Ui(QtWidgets.QMainWindow):
             time.sleep(1)
 
 
-
 # todo перенести все методы
 @bot.message_handler(commands=['start'])
 def button_message(message):
     if len(order_list) == 0:
         order_list.append(shaurma.shaurma(1, [], bot, message))
 
+
 def choice_additives(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = KeyboardButton("добавка сыр")
-    item2 = KeyboardButton("добавка мясо")
-    item3 = KeyboardButton("добавка огурцы")
-    item5 = KeyboardButton("добавка халапеньо")
-    item6 = KeyboardButton("добавка сухарики")
-    item7 = KeyboardButton("добавка помидоры")
-    item8 = KeyboardButton("добавка картофель фри")
-    item9 = KeyboardButton("добавка кукурза")
-    item10 = KeyboardButton("добавка ананас")
-    item11 = KeyboardButton("добавка соус острый")
-    item12 = KeyboardButton("/finish")
-    markup.add(item1)
-    markup.add(item2)
-    markup.add(item3)
-    markup.add(item5)
-    markup.add(item6)
-    markup.add(item7)
-    markup.add(item8)
-    markup.add(item9)
-    markup.add(item10)
-    markup.add(item11)
-    markup.add(item12)
+
+    markup.add(KeyboardButton(f"добавка сыр {cheese_symbol}"))
+    markup.add(KeyboardButton(f"добавка мясо {chicken_symbol}"))
+    markup.add(KeyboardButton(f"добавка огурцы {cucumber_symbol}"))
+    markup.add(KeyboardButton(f"добавка халапеньо {peper_symbol}"))
+    markup.add(KeyboardButton(f"добавка сухарики {bread_symbol}"))
+    markup.add(KeyboardButton(f"добавка помидоры {tomato_symbol}"))
+    markup.add(KeyboardButton(f"добавка картофель фри {fries_symbol}"))
+    markup.add(KeyboardButton(f"добавка кукурза {corn_symbol}"))
+    markup.add(KeyboardButton(f"добавка ананас {pineapple_symbol}"))
+    markup.add(KeyboardButton(f"добавка соус острый {fire_symbol}"))
+    markup.add(KeyboardButton("/finish"))
+
     bot.send_message(message.chat.id, 'Выберите что добавить', reply_markup=markup)
+
 
 def scanorder():
     while True:
@@ -113,56 +118,67 @@ def scanorder():
 @bot.message_handler(commands=['finish'])
 def final(message):
     if len(order_list) == 0:
-        order_list.append(shaurma.shaurma(1, [], bot,message))
+        order_list.append(shaurma.shaurma(1, [], bot, message))
     print(order_list[0].name, "Добавки:")
     for additive in order_list[0].additives:
         print(additive, end=" ")
     print()
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    item9 = KeyboardButton("да")
-    item10 = KeyboardButton("нет")
-    markup.add(item9)
-    markup.add(item10)
-    bot.send_message(message.chat.id,f"Ваша шаурма: {order_list[0].name}")
-    bot.send_message(message.chat.id, f"Ваши добавки: {order_list[0].additives}")
+
+    markup.add(KeyboardButton("да"))
+    markup.add(KeyboardButton("нет"))
+
+    bot.send_message(message.chat.id, f"Ваша шаурма: {order_list[0].name}\n"
+                                      f"Ваши добавки: {order_list[0].additives}\n"
+                                      f"Ждём Вас по адресу Жемчужная, 34 к2 киоск!\n" \
+                                      f"Приятного аппетита! Приходите ещё!")
+
     bot.send_message(message.chat.id, 'Подтвердить заказ?', reply_markup=markup)
 
 
 @bot.message_handler(content_types='text')
 def choice_shaurma(message):
     if len(order_list) == 0:
-        order_list.append(shaurma.shaurma(1, [], bot,message))
+        order_list.append(shaurma.shaurma(1, [], bot, message))
     global cur
-    #todo Устойчивость к поломке(преждевременного завершения)(можно ещё задание на предотвращение левых добавок) - Тимур, Алексей
-    if message.text == "да":
-        print("Заказ принят")
-        to_do.append(order_list[0])
-        markup = ReplyKeyboardMarkup(resize_keyboard=True)
-        item10 = KeyboardButton("/start")
-        markup.add(item10)
-        bot.send_message(message.chat.id, 'Сделать ешё 1 заказ?', reply_markup=markup)
-        del order_list[0]
-    elif message.text == "нет":
-        print("Заказ отменён")
-        markup = ReplyKeyboardMarkup(resize_keyboard=True)
-        item10 = KeyboardButton("/start")
-        markup.add(item10)
-        bot.send_message(message.chat.id, 'Сделать заказ?', reply_markup=markup)
-        del order_list[0]
+    # todo Устойчивость к поломке(преждевременного завершения)(можно ещё задание на предотвращение левых добавок) - Тимур, Алексей
+    if len(message.text) != 0:
+        if message.text == "да":
+            print("Заказ принят")
+            to_do.append(order_list[0])
+            markup = ReplyKeyboardMarkup(resize_keyboard=True)
+            item10 = KeyboardButton("/start")
+            markup.add(item10)
+            bot.send_message(message.chat.id, 'Сделать ешё 1 заказ?', reply_markup=markup)
+            del order_list[0]
+
+        elif message.text == "нет":
+            print("Заказ отменён")
+            markup = ReplyKeyboardMarkup(resize_keyboard=True)
+            item10 = KeyboardButton("/start")
+            markup.add(item10)
+            bot.send_message(message.chat.id, 'Сделать заказ?', reply_markup=markup)
+            del order_list[0]
+
+        else:
+
+            item, name = message.text.split()[:-1]
+            if item == "шаурма":
+                order_list[0].name = name
+                choice_additives(message)
+            if item == "добавка":
+                order_list[0].additives.append(name)
+
     else:
-        if message.text[0:6] == "шаурма":
-            type, name = message.text.split()
-            order_list[0].name = name
-            choice_additives(message)
-        if message.text[0:7] == "добавка":
-            type, name = message.text.split()
-            order_list[0].additives.append(name)
+        bot.send_message(message.chat.id, 'Жду ответ!')
+
+
 thread = Thread(target=scanorder, daemon=True)
 thread.start()
 
-
 thread_bot = Thread(target=bot.infinity_polling, daemon=True)
 thread_bot.start()
+
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 thread_update = Thread(target=window.update, daemon=True)
--- shaurma.py
+++ shaurma.py
@@ -1,24 +1,73 @@
-from telebot import types
-#todo патерн билдер
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+# todo патерн билдер
+
+heart_symbol = u"\u2764"
+cheese_symbol = u"\U0001F9C0"
+fitness_symbol = u"\U0001F3C3\u200D\u2642\uFE0F"
+chicken_symbol = u"\U0001F357"
+pekin_symbol = u"\U0001F96C"
+burger_symbol = u"\U0001F354"
+
+
 class shaurma:
-    def __init__(self, name, additives,bot,message):
+    def __init__(self, name, additives, bot, message):
         self.name = name
         self.additives = additives
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton("шаурма Цезарь")
-        item2 = types.KeyboardButton("шаурма Классическая")
-        item3 = types.KeyboardButton("шаурма Сырная")
-        item4 = types.KeyboardButton("шаурма Фитнес")
-        item5 = types.KeyboardButton("шаурма В булке")
-        markup.add(item1)
-        markup.add(item2)
-        markup.add(item3)
-        markup.add(item4)
-        markup.add(item5)
+
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+        # markup.add(KeyboardButton(f"{pekin_symbol} шаурма Цезарь"))
+        # markup.add(KeyboardButton(f"{chicken_symbol} шаурма Классическая"))
+        # markup.add(KeyboardButton(f"{cheese_symbol} шаурма Сырная"))
+        # markup.add(KeyboardButton(f"{fitness_symbol} шаурма Фитнес"))
+        # markup.add(KeyboardButton(f"{burger_symbol} шаурма В булке"))
+
+        markup.add(KeyboardButton(f"шаурма Цезарь {pekin_symbol}"))
+        markup.add(KeyboardButton(f"шаурма Классическая {chicken_symbol}"))
+        markup.add(KeyboardButton(f"шаурма Сырная {cheese_symbol}"))
+        markup.add(KeyboardButton(f"шаурма Фитнес {fitness_symbol}"))
+        markup.add(KeyboardButton(f"шаурма В булке {burger_symbol}"))
+
+        bot.send_message(message.chat.id,
+                         'Привет! Мы открыты каждый день с 9:00 до 1:00 по адресу Жемчужная, 34 к2 киоск.\n'
+                         'Также у нас работает доставка с 9:00 до 1:00 в Яндекс Еде!')
+
+        bot.send_message(message.chat.id, 'Перед Вами наше меню:')
+
+        bot.send_message(message.chat.id, f'{pekin_symbol} шаурма Цезарь:\n'
+                                          'лаваш, мясо, помидоры .... *ингредиенты*\n'
+                                          'маленькая (150 рубллей/500 гр),\n'
+                                          'средняя (175 рубллей/600 гр),\n'
+                                          'большая (200 рубллей/700 гр)\n')
+
+        bot.send_message(message.chat.id, f'{chicken_symbol} шаурма Классическая:\n'
+                                          'лаваш, мясо, помидоры .... *ингредиенты*\n'
+                                          'маленькая (150 рубллей/500 гр),\n'
+                                          'средняя (175 рубллей/600 гр),\n'
+                                          'большая (200 рубллей/700 гр)\n')
+
+        bot.send_message(message.chat.id, f'{cheese_symbol} шаурма Сырная:\n'
+                                          'лаваш, мясо, помидоры .... *ингредиенты*\n'
+                                          'маленькая (150 рубллей/500 гр),\n'
+                                          'средняя (175 рубллей/600 гр),\n'
+                                          'большая (200 рубллей/700 гр)\n')
+
+        bot.send_message(message.chat.id, f'{fitness_symbol} шаурма Фитнес:\n'
+                                          'лаваш, мясо, помидоры .... *ингредиенты*\n'
+                                          'маленькая (150 рубллей/500 гр),\n'
+                                          'средняя (175 рубллей/600 гр),\n'
+                                          'большая (200 рубллей/700 гр)\n')
+
+        bot.send_message(message.chat.id, f'{burger_symbol} шаурма В булке:\n'
+                                          'лаваш, мясо, помидоры .... *ингредиенты*\n'
+                                          'маленькая (150 рубллей/500 гр),\n'
+                                          'средняя (175 рубллей/600 гр),\n'
+                                          'большая (200 рубллей/700 гр)\n')
+
         bot.send_message(message.chat.id, 'Выберите что хотите', reply_markup=markup)
-        # choice_shaurma(message)
 
     def __str__(self):
-        sting = f"Ваша шаурма: {self.name} " + f"Ваши добавки: {self.additives}"
+        sting = f"Шаурма: {self.name}\n" \
+                f"Добавки: {self.additives}\n"
         return sting
-
